388f0ee65839b1bcd66cc76e2e23b9ee21d9e1aa
Paula Tulis <tulis.paula@gmail.com>
2019-03-17 19:40:23 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,11 @@
-def hello_t(names)
-  i = 0 
-  while i< names.length 
-  yield (names[i])
-  i = i+1 
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
 end
-names
-end 
 
